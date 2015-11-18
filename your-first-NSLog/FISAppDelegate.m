de7fa1a7fc7a71138b4475c5e0d6b7c1a39e1fc3
Alex Bargmann <abargmann@gmail.com>
2015-11-18 14:44:51 -0500

    printing statement

--- your-first-NSLog/FISAppDelegate.m
+++ your-first-NSLog/FISAppDelegate.m
@@ -5,13 +5,7 @@
 @implementation FISAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-
-    /**
-    
-     * Hey! Write your code here! :D
-     
-     */
-    
+    NSLog(@"Hello, World!");
     // do not alter
     return YES;  //
 }   ///////////////
